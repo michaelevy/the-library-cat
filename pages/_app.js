bbbb12134ad44cdd6eb6332da9003e6453a54e42
michael-levy <michaelfeehanlevy@gmail.com>
2021-08-23 14:20:38 +1200

    styled-components fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,13 @@
 import "../styles/globals.css";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
-import { useEffect, useState } from "react";
 function MyApp({ Component, pageProps }) {
-  const [mounted, hasMounted] = useState(false);
   const router = useRouter();
-  useEffect(() => {
-    hasMounted(true);
-  }, [mounted]);
+
   return (
-    mounted && (
-      <Layout route={router.route}>
-        <Component {...pageProps} />
-      </Layout>
-    )
+    <Layout route={router.route}>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
