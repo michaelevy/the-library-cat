9b01c76f4d06a2cf375f3c2930fd1cfc42eb2512
michael-levy <michaelfeehanlevy@gmail.com>
2021-08-23 14:05:12 +1200

    mounted fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
+import { useEffect, useState } from "react";
 function MyApp({ Component, pageProps }) {
+  const [mounted, hasMounted] = useState(false);
   const router = useRouter();
+  useEffect(() => {
+    hasMounted(true);
+  }, [mounted]);
   return (
     <Layout route={router.route}>
-      <Component {...pageProps} />
+      {mounted && <Component {...pageProps} />}
     </Layout>
   );
 }
