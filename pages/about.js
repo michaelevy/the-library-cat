90777873811ce982332678853280c63e01c4d5ae
michael-levy <michaelfeehanlevy@gmail.com>
2021-08-31 17:33:41 +1200

    edited about and contact page

--- pages/about.js
+++ pages/about.js
@@ -9,14 +9,18 @@ export default function about() {
 
       <p>
         I'm Michael. I read books and write code. This website is the happy
-        intersection of the two. It doesn't look very good, because I don't know
-        anything about design. If you do, please head over to 'contact' and
-        proceed with the ritual.
+        intersection of the two.
       </p>
       <p>
+        I read almost exclusively sci-fi and fantasy, more out of habit than
+        design. My favourite author is Sanderson, which is awfully boring of me.
+      </p>
+      <p>
+        <br />
+        <br />
         <em>
           "In ancient times, cats were worshipped as gods; they have not
-          forgotten this."{" "}
+          forgotten this." <br />
         </em>
         - Terry Pratchett
       </p>
