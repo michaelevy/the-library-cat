ced64093b9568e185dfd1ca5fe3d07255fa4d522
michael-levy <michaelfeehanlevy@gmail.com>
2022-07-06 12:28:47 +1200

    increase page width

--- components/PageText.js
+++ components/PageText.js
@@ -7,12 +7,12 @@ const PageTextStyle = styled.article`
   font-family: var(--body-serif);
   margin-left: auto;
   margin-right: auto;
-  max-width: 40%;
+  max-width: 50%;
   text-align: left;
   color: var(--grey);
   line-height: 30px;
 
-  @media only screen and (max-width: 900px) {
+  @media only screen and (max-width: 1300px) {
     max-width: 90%;
   }
 `;
