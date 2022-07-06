5ec04bde7c3b857b02e94649d542e75119ab46c8
michael-levy <michaelfeehanlevy@gmail.com>
2022-07-06 15:46:10 +1200

    switch Header.js from jsx to styled-components

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
+import styled from "styled-components";
 
 let subtitles = [
   "So many books, so little time",
@@ -24,25 +25,16 @@ let subtitle = null;
 function Subtitle({ children }) {
   return (
     <AnimatePresence>
-      <motion.h2
+      <HeadingStyle
         key={children}
         exit={{ opacity: 0 }}
-        style={{
-          position: "absolute",
-          left: 0,
-          right: 0,
-          marginLeft: "auto",
-          marginRight: "auto",
-          width: "100%",
-          fontSize: "calc(1em * var(--text-ratio))",
-        }}
         transition={{ duration: "1" }}
         initial={{ opacity: 0 }}
         id="subtitle"
         animate={{ opacity: 1 }}
       >
         {children}
-      </motion.h2>
+      </HeadingStyle>
     </AnimatePresence>
   );
 }
@@ -60,7 +52,7 @@ export default function Header() {
   }
   subtitle = newSub;
   return (
-    <header key="header">
+    <HeaderStyle key="header">
       <h1>
         <Link href="/">
           <a id="title">
@@ -73,59 +65,70 @@ export default function Header() {
       <div style={{ height: "50px" }}>
         <Subtitle>{subtitle}</Subtitle>
       </div>
-
-      <style jsx>{`
-        --text-ratio: 1;
-        @media only screen and (max-width: 480px) {
-          --text-ratio: 0.75;
-        }
-        header {
-          width: 90%;
-          text-align: center;
-          margin: 20px auto;
-          color: var(--grey);
-        }
-        header a {
-          text-decoration: none;
-          display: inline-block;
-          color: var(--dark-sec);
-        }
-        header h1 {
-          will-change: transform;
-          transition: 0.8s;
-          margin: 0;
-          margin-bottom: 4px;
-        }
-        header h1:hover {
-          transition: 0.15s;
-          transform: scale(1.1);
-        }
-        header span {
-          padding: 0 10px;
-          line-height: 1em;
-          font-size: calc(1em * var(--text-ratio));
-        }
-        header span:first-child {
-          font-weight: 400;
-        }
-        header span:nth-last-child(2) {
-          font-size: calc(1.5em * var(--text-ratio));
-          font-weight: 800;
-          font-family: var(--heading);
-        }
-        #title {
-          display: flex;
-          flex-direction: row;
-          flex-wrap: wrap;
-          width: 80%;
-          margin: 0 auto;
-          justify-content: center;
-          align-items: center;
-        }
-        #title:hover {
-          cursor: pointer;
-        }
-      `}</style>
-    </header>
+    </HeaderStyle>
   );
 }
+
+const HeadingStyle = styled(motion.h2)`
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  font-size: 1em;
+  @media only screen and (max-width: 800px) {
+    font-size: 0.75em;
+  }
+`;
+const HeaderStyle = styled.header`
+  --text-ratio: 1;
+  @media only screen and (max-width: 480px) {
+    --text-ratio: 0.75;
+  }
+  width: 90%;
+  text-align: center;
+  margin: 20px auto;
+  color: var(--grey);
+
+  a {
+    text-decoration: none;
+    display: inline-block;
+    color: var(--dark-sec);
+  }
+  h1 {
+    will-change: transform;
+    transition: 0.8s;
+    margin: 0;
+    margin-bottom: 4px;
+  }
+  h1:hover {
+    transition: 0.15s;
+    transform: scale(1.1);
+  }
+  span {
+    padding: 0 10px;
+    line-height: 1em;
+    font-size: calc(1em * var(--text-ratio));
+  }
+  span:first-child {
+    font-weight: 400;
+  }
+  span:nth-last-child(2) {
+    font-size: calc(1.5em * var(--text-ratio));
+    font-weight: 800;
+    font-family: var(--heading);
+  }
+  #title {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 80%;
+    margin: 0 auto;
+    justify-content: center;
+    align-items: center;
+  }
+  #title:hover {
+    cursor: pointer;
+  }
+`;
