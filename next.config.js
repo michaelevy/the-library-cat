70200440b6610329e4648fae578da398ef5ec3b3
Michael Levy <michaelfeehanlevy@gmail.com>
2024-09-07 17:02:30 +1200

    Modernise filtering controls

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+const path = require('path');
+
 module.exports = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+  webpack: (config) => {
+    config.resolve.alias["react/jsx-runtime"] = path.join(
+      __dirname,
+      "/node_modules/react/jsx-runtime.js"
+    );
+    return config;
+  }
 };
